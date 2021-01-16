5ae1f5d05c88d843bfc8602e862fe7c6eca576b5
Theethawat Savastham <5910110150@psu.ac.th>
2021-01-16 14:58:44 +0700

    ปรับหน้า Home

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,13 +4,13 @@ import TDCLogo from "../Assets/TDC Logo black.png";
 
 const AppNavBar = () => {
   return (
-    <div className='w-full bg-shamrock-100 py-2'>
+    <div className='w-full bg-shamrock-100 py-2 shadow-sm'>
       <div className='container mx-auto px-4'>
         <div className='flex'>
           {/* <img className='h-8 ' src={TDCLogo} alt='The Duck Creator Logo' /> */}
           <h2 className='text-2xl font-semibold text-white font-sans ml-4'>
             {" "}
-            TDC - เดอะดักส์ครีเอเตอร์
+            ระบบบันทึกการทานยา
           </h2>
         </div>
       </div>
